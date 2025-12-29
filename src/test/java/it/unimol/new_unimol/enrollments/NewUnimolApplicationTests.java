6e176755f3043c97cfe64758f44d34eae4c8f893
Antonio <desantisantonio26@gmail.com>
2025-12-29 16:40:26 +0100

    Update file di test NewUnimolApplicationTests.java

--- src/test/java/it/unimol/new_unimol/enrollments/NewUnimolApplicationTests.java
+++ src/test/java/it/unimol/new_unimol/enrollments/NewUnimolApplicationTests.java
@@ -1,13 +1,54 @@
 package it.unimol.new_unimol.enrollments;
 
+import it.unimol.new_unimol.enrollments.service.AdminService;
+import it.unimol.new_unimol.enrollments.service.StudentService;
+import it.unimol.new_unimol.enrollments.service.TeacherService;
+import it.unimol.new_unimol.enrollments.service.TokenJWTService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class NewUnimolApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private StudentService studentService;
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private TokenJWTService tokenJWTService;
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
+    }
+
+    @Test
+    void testServiciesAreNotNull() {
+        assertAll(
+                () -> assertNotNull(studentService, "StudentService dovrebbe essere caricato"),
+                () -> assertNotNull(teacherService, "TeacherService dovrebbe essere caricato"),
+                () -> assertNotNull(adminService, "AdminService dovrebbe essere caricato"),
+                () -> assertNotNull(tokenJWTService, "TokenJWTService dovrebbe essere caricato")
+        );
+    }
+
+    @Test
+    void testApplicationContextContainsBeans() {
+        assertTrue(applicationContext.containsBean("studentService"));
+        assertTrue(applicationContext.containsBean("teacherService"));
+        assertTrue(applicationContext.containsBean("adminService"));
     }
 
 }
